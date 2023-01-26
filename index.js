478d67ce644c0ad745203bfc883f02025a931186
eric <ericdonnelly4@gmail.com>
2023-01-26 15:45:34 -0500

    Completed the main part of the assignment

--- index.js
+++ index.js
@@ -2,4 +2,40 @@ const rs = require('readline-sync');
 
 
 
+const validOperation = ["+","-","*","/"];
+console.log("Hello")
+
+let operation= rs.question('What operation would you like to preform? ',
+    {
+        limit:validOperation,
+        limitMessage:"That is not a valid operation. Please enter one of the following : $<limit>"
+    });
+
+if (!validOperation.includes(operation)){
+    console.log();
+
+} else {
+    let firstNum = rs.questionInt("Please enter the first number? ",{limitMessage:"This is not a number"});
+    let secondNum = rs.questionInt("Please enter the second number? ",{limitMessage:"This is not a number"});
+    let result = findResult(operation);
+
+    function findResult (operation) {
+        switch (operation) {
+            case "+":
+                return firstNum + secondNum;
+            case "-":
+                return firstNum - secondNum;
+            case "/":
+                return firstNum / secondNum;
+            case "*":
+                return firstNum * secondNum;
+            default:
+                return "Operation not completed";
+        }
+    }
+
+    console.log(`The answer for your equation is ${firstNum} ${operation} ${secondNum} = ${result}`)
+
+}
+
 
